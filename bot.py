9a10d98db32a1e9f3d301b7b857e2172816cf905
LarryLai <LarryHZLai@mobiledrivetech.com>
2023-09-18 14:51:15 +0800

    Update function to new version - add fortune teller

--- bot.py
+++ bot.py
@@ -1,41 +1,92 @@
+import datetime
+
 from discord.ext import commands
 import discord
 import trigger
 import os
 import json
-#import keep_alive
+# import keep_alive
 import random
 
 intents = discord.Intents.all()
 
+bot = commands.Bot(command_prefix="?", intents=intents)
 
-bot = commands.Bot(command_prefix="?",intents=intents)
+TEMP_LIST = []
+FORTUNE_LIST = [
+    '小吉', '吉', '大吉', '小凶', '凶', '大凶',
+    '吉娃娃的那種吉', '你受到了金在熙的祝福!', "特吉列車~~bubu~~"
+]
+FORTUNE_RATIO_LIST = [30, 15, 4, 30, 15, 4, 1 / 3, 1 / 3, 1 / 3]
 
 with open('setting.json', 'r') as jfile:
     jdata = json.load(jfile)
 
 
+def is_mod(user_id):
+    return user_id in jdata['MOD_ID']
+
+
+class ReceivedText:
+    def __init__(self, author, content):
+        self.timestamp = datetime.datetime.now()
+        self.author = author
+        self.content = content
+
+
 @bot.command()
 async def unload(ctx, extension):
-    await bot.unload_extension(f'cmd.{extension}')
-    await ctx.send(f'{extension} is unloaded')
+    if is_mod(ctx.author.id):
+        await bot.unload_extension(f'cmd.{extension}')
+        await ctx.send(f'{extension} is unloaded')
 
 
 @bot.command()
 async def reload(ctx):
-    for Filename in os.listdir('./cmds'):
-        if Filename.endswith('.py'):
-            await bot.reload_extension(f'cmds.{Filename[:-3]}')
-            await ctx.send(f'{Filename[:-3]} is loaded')
+    if is_mod(ctx.author.id):
+        for Filename in os.listdir('./cmds'):
+            if Filename.endswith('.py'):
+                await bot.reload_extension(f'cmds.{Filename[:-3]}')
+                await ctx.send(f'{Filename[:-3]} is loaded')
+
+
+@bot.event
+async def on_member_join(member):
+    embed = discord.Embed(title=":loudspeaker: 汪!汪汪!!", type="rich", color=0x8400ff,
+                          description=f"歡迎<@{member.id}> 加入十九層開發建設!\n"
+                                      f"請記得詳閱公會規定\n"
+                                      f"如果想找攻略，這邊也有我們整理過的攻略，歡迎參考",
+                          )
+    embed.set_thumbnail(url="https://img.freepik.com/premium-photo/close-up-angry-chihuahua-growling-2-years-old"
+                            "-front-white-wall_191971-1367.jpg?w=826")
+    view = discord.ui.View()
+    link_to_rule_btn = discord.ui.Button(label="公會規定",
+                                         url="https://discord.com/channels/992809924230914088/996643108471193691")
+    link_to_doc_btn = discord.ui.Button(label="遊戲攻略",
+                                        url="https://docs.google.com/spreadsheets/d/176GT6s7ulPK3Hj18UZppDSQRD"
+                                            "-KtgMOjlagjhhS8fNM/edit#gid=1195001495")
+    view.add_item(link_to_rule_btn)
+    view.add_item(link_to_doc_btn)
+    await bot.get_channel(jdata["ID_CHANNEL_BOT_WELCOME"]).send(view=view, embed=embed)
+
+
+@bot.event
+async def on_member_remove(member):
+    await bot.get_channel(jdata["ID_CHANNEL_BOT_WELCOME"]).send(f"{member.id} has left")
 
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
+    try:
+        channel_name = message.channel.name
+    except AttributeError:
+        channel_name = "dm"
+    print(f"{message.author.display_name} in {channel_name}: {message.content}")
     for row in trigger.TRIGGER_LIST:
-        if message.content == row['key']:
-            await message.channel.send(row['value'])
+        if row['key'] in message.content:
+            await message.reply(row['value'])
     if message.content.startswith("!add"):
         temp = message.content.split(" ")
         if len(temp) < 3:
@@ -47,11 +98,31 @@ async def on_message(message):
                 value = value + " " + str(temp[i])
             trigger.add_trigger(key, value)
             await message.channel.send("指令已新增, " + key + ", " + value)
-    if message.content.endswith("運勢"):
-        ret_list = ['吉','小吉','大吉','吉娃娃的那種吉','你受到了金在熙的祝福!','凶','大凶','小凶']
-        ratio_list = [30,15,5,1,1,30,5,15]
-        ret = random.choices(ret_list,weights=ratio_list)
-        await message.channel.send(ret[0])
+    if message.content.endswith("運勢") and message.channel.id in [jdata["ID_CHANNEL_BOT_LOTTERY"],
+                                                                   jdata["ID_CHANNEL_BOT_TESTER"]]:
+        received_content = ReceivedText(message.author.display_name, message.content)
+        is_duplicate_asking = False
+        for content in TEMP_LIST:
+            time_delta = received_content.timestamp - content.timestamp
+            print(time_delta)
+            if time_delta < datetime.timedelta(seconds=7200) \
+                    and content.author == received_content.author and content.content == received_content.content:
+                is_duplicate_asking = True
+        if not is_duplicate_asking:
+            ret = random.choices(FORTUNE_LIST, weights=FORTUNE_RATIO_LIST)[0]
+            image_list = os.listdir("images")
+            image_ret = random.choices(image_list)[0]
+            print(message.author.display_name + " : " + message.content)
+            print(ret + ", " + image_ret)
+            file = discord.File("images" + os.sep + image_ret, filename="image.png")
+            embed = discord.Embed(type="rich", title=f"{message.content}是...", description=ret
+                                  )
+            embed.set_thumbnail(url=f"attachment://{file.filename}")
+            embed.set_author(name="十九層地獄神犬占卜")
+            await message.reply(file=file, embed=embed)
+            TEMP_LIST.append(received_content)
+        else:
+            await message.reply(r"問第二次就是大凶 No doubt, bitch")
 
     await bot.process_commands(message)
 
@@ -61,12 +132,14 @@ async def on_ready():
     print("Now I'm logging as ", bot.user)
     trigger.init_trigger()
 
-
     for Filename in os.listdir('./cmds'):
         if Filename.endswith('.py'):
             await bot.load_extension(f'cmds.{Filename[:-3]}')
 
+
 if __name__ == "__main__":
-    #keep_alive.keep_alive()
+    # keep_alive.keep_alive()
     print('bot starting')
-    bot.run(jdata['TOKEN'])  #token
+    import dotenv
+    dotenv.load_dotenv()
+    bot.run(os.getenv('TOKEN'))  # token
--- trigger.py
+++ trigger.py
@@ -1,19 +1,43 @@
 import csv
 
-
-TRIGGER_CSV="trigger.csv"
+TRIGGER_CSV = "trigger.csv"
 TRIGGER_LIST = []
 
+
 def init_trigger():
-	with open(TRIGGER_CSV, newline='',encoding='UTF-8') as csvfile:
-		# 讀取 CSV 檔案內容
-		rows = csv.DictReader(csvfile)
-		for row in rows:
-			TRIGGER_LIST.append(row)
-			
-def add_trigger(key,value):
-	with open(TRIGGER_CSV, "a+") as csvfile:
-		r = csv.writer(csvfile)
-		r.writerow("")
-		r.writerow([key,value])
-	init_trigger()
+    trigger_list = []
+    with open(TRIGGER_CSV, newline='', encoding='UTF-8') as csvfile:
+        # 讀取 CSV 檔案內容
+        rows = csv.DictReader(csvfile)
+        for row in rows:
+            trigger_list.append(row)
+    global TRIGGER_LIST
+    TRIGGER_LIST = trigger_list
+
+
+def add_trigger(key, value):
+    init_trigger()
+    with open(TRIGGER_CSV, "a+", newline='', encoding='UTF-8') as csvfile:
+        r = csv.writer(csvfile)
+        r.writerow([key, value])
+    init_trigger()
+
+
+def remove_trigger(key):
+    init_trigger()
+    print(TRIGGER_LIST)
+    for row in TRIGGER_LIST:
+        if row['key'] == key:
+            TRIGGER_LIST.pop(TRIGGER_LIST.index(row))
+    print(TRIGGER_LIST)
+    with open(TRIGGER_CSV, "w", newline='', encoding='UTF-8') as csvfile:
+        r = csv.DictWriter(csvfile, fieldnames=["key", "value"])
+        r.writeheader()
+        for row in TRIGGER_LIST:
+            r.writerow(row)
+
+
+if __name__ == '__main__':
+    # add_trigger("test","312")
+    # add_trigger("123","123")
+    remove_trigger("test")
